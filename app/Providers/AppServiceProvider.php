c528fcf66b4b17da6d96cdfad287b4b10fa2b1f5
Antoniya <apstavreva@gmail.com>
2020-02-12 07:11:06 +0200

    force https on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+        
         Schema::defaultStringLength(191);
     }
 }
